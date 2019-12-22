ecb920cee7c2b671caafa6389254874061df6d3e
hazaldogan <hazl.dogann@gmail.com>
2019-12-22 12:38:33 +0300

    Cart ve CartItem Sınıfları Ekleme

---
+++ MVCAuthenticationTekrar/Controllers/ShoppingController.cs
@@ -0,0 +1,26 @@
+﻿using MVCAuthenticationTekrar.AuthenticationClasses;
+using MVCAuthenticationTekrar.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAuthenticationTekrar.Controllers
+{
+    [MemberAuthentication]
+    public class ShoppingController : Controller
+    {
+        MyContext db;
+
+        public ShoppingController()
+        {
+            db = new MyContext();
+        }
+        // GET: Shopping
+        public ActionResult ShoppingList()
+        {
+            return View();
+        }
+    }
+}
